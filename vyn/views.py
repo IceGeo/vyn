10eef6302e693dc3cb87e1247477c901714437a9
IceGeo <lucasbunel@gmail.com>
2023-03-08 10:04:03 +0000

    Scanner ARP

--- vyn/views.py
+++ vyn/views.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from scapy.all import ARP, Ether, srp
 
 app = Flask(__name__)
 app.config.from_object("config")
@@ -10,30 +9,6 @@ def index():
 
 @app.route('/monitoring/')
 def monitor():
-    target_ip = "192.168.1.1/24"
-    # IP Address for the destination
-    # create ARP packet
-    arp = ARP(pdst=target_ip)
-    # create the Ether broadcast packet
-    # ff:ff:ff:ff:ff:ff MAC address indicates broadcasting
-    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
-    # stack them
-    packet = ether/arp
-
-    result = srp(packet, timeout=3, verbose=0)[0]
-
-    # a list of clients, we will fill this in the upcoming loop
-    clients = []
-
-    for sent, received in result:
-        # for each response, append ip and mac address to `clients` list
-        clients.append({'ip': received.psrc, 'mac': received.hwsrc})
-
-    # print clients
-    print("Available devices in the network:")
-    print("IP" + " "*18+"MAC")
-    for client in clients:
-        print("{:16}    {}".format(client['ip'], client['mac']))
     return "Hi there"
 
 if __name__ == "__main__":
