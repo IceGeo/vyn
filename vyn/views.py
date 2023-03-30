98c62f50c933e4a9c77bdf8558b0184922ceaf58
IceGeo <lucasbunel@gmail.com>
2023-03-30 22:09:32 +0000

    adding web page

--- vyn/views.py
+++ vyn/views.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, send_from_directory, request
-
+from vyn.discovers import scan
 app = Flask(__name__, static_folder='static', static_url_path='')
 app.config.from_object("config")
 
@@ -9,8 +9,13 @@ def index():
     return render_template('home.html')
 
 
-@app.route('/managment/')
+@app.route('/managment/', methods=['GET', 'POST'])
 def managment():
+    if request.method == 'POST':
+        ip = request.form.get('adresse_ip')
+        iface = request.form.get('interface_reseau')
+        scanner = scan(ip, iface)
+        scanner.arp()      
     return render_template('managment.html')
 
 
